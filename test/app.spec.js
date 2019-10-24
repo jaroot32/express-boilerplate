055ad256eca750f21bec085d90c9684e8fbed388
Jonathan Root <jonathanmbp2019@Jonathans-MacBook-Pro.local>
2019-10-24 15:48:27 -0700

    first commit

---
+++ test/app.spec.js
@@ -0,0 +1,12 @@
+const app = require('../src/app')
+
+global.expect = expect
+global.supertest = supertest
+
+describe('App', () => {
+  it('GET / responds with 200 containing "Hello, world!"', () => {
+    return supertest(app)
+      .get('/')
+      .expect(200, 'Hello, world!')
+  })
+})
