18da1eed84c9e10457025827b224c644910770f5
Jonathan Root <jonathanmbp2019@Jonathans-MacBook-Pro.local>
2020-01-05 02:54:41 -0800

    test

--- test/app.spec.js
+++ test/app.spec.js
@@ -1,8 +1,5 @@
 const app = require('../src/app')
 
-global.expect = expect
-global.supertest = supertest
-
 describe('App', () => {
   it('GET / responds with 200 containing "Hello, world!"', () => {
     return supertest(app)
